7873855af3eeba1db98b99e3b2cbb84ab2482329
ihtadalutfi <71589785+ihtadalutfi@users.noreply.github.com>
2020-09-22 20:29:00 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int masukan = scanner.nextInt();
+        int perhitungan =masukan * masukan;
+        System.out.println(perhitungan);
+    }
+}
